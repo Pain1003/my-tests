13a2421d858759ed3571417bb48e7b009a1bae24
Ivan Panayotov <pain1003@gmail.com>
2026-05-06 15:05:23 +0300

    arch: implemented Page Object Model for login

---
+++ pom-login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+
+test('Login using POM', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await page.goto('https://saucedemo.com');
+
+    // No more messy locators here! Just "Human" actions:
+    await loginPage.login('standard_user', 'secret_sauce');
+
+    await expect(page).toHaveURL(/.*inventory.html/);
+});
