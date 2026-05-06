e8aaab0e21e5d4eae4e2dda9709c70433ceb8de5
Ivan Panayotov <pain1003@gmail.com>
2026-05-06 15:31:19 +0300

    reverted changes

--- pom-login.spec.ts
+++ pom-login.spec.ts
@@ -7,10 +7,7 @@ test('Login using POM', async ({ page }) => {
     await page.goto('https://saucedemo.com');
 
     // No more messy locators here! Just "Human" actions:
-    await loginPage.login(
-        process.env.SAUCE_USERNAME!,
-        process.env.SAUCE_PASSWORD!
-    );
+    await loginPage.login('standard_user', 'secret_sauce');
 
     await expect(page).toHaveURL(/.*inventory.html/);
 });
